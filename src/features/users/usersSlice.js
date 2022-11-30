e5d965d9e4cada20bf4991338b96ea9d3c39df65
vishnuSekhar <vishnusekhar96@gmail.com>
2022-11-30 18:20:25 +0530

    Boilerplate code for slicing

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,1 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: "0", name: "Vishnu Sekhar" },
+  { id: "1", name: "Aaron Mankhe" },
+  { id: "2", name: "Irene Adler" },
+];
+
+const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+});
+export const selectAllUsers = (state) => state.users;
+export default userSlice.reducer;
