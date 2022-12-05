240c0241377938d55dd4eead17255ab39df061fd
vishnuSekhar <vishnusekhar96@gmail.com>
2022-12-05 11:32:30 +0530

    Add reaction array to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,16 +1,22 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
-
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 const PostsList = () => {
   const posts = useSelector(selectAllPosts); //dont call the function
-  const renderedPosts = posts.map((post, index) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date)); //slice to generate a shallow copy of the array
+  const renderedPosts = orderedPosts.map((post, index) => (
     <article key={index}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p>
         <PostAuthor userId={post.userId} />
+        <TimeAgo timeStamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   ));
   return (
