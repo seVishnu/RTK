7cca7ac926a5123c2153c2951e6d5e48965a6190
vishnuSekhar <vishnusekhar96@gmail.com>
2022-12-06 09:53:15 +0530

    add axios to hydrate store with server data

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,42 +1,56 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
+import axios from "axios";
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning RTK",
-    content: "Lets do this",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: "2",
-    title: "Slices",
-    content: "Like an orange",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
-
+// const initialState = [
+//   {
+//     id: "1",
+//     title: "Learning RTK",
+//     content: "Lets do this",
+//     date: sub(new Date(), { minutes: 10 }).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       wow: 0,
+//       heart: 0,
+//       rocket: 0,
+//       coffee: 0,
+//     },
+//   },
+//   {
+//     id: "2",
+//     title: "Slices",
+//     content: "Like an orange",
+//     date: sub(new Date(), { minutes: 5 }).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       wow: 0,
+//       heart: 0,
+//       rocket: 0,
+//       coffee: 0,
+//     },
+//   },
+// ];
+const initialState = {
+  posts: [],
+  status: "idle", //'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  try {
+    const response = await axios.get(POSTS_URL);
+    return [...response.data];
+  } catch (error) {
+    return error.message;
+  }
+});
 const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload); //immer JS makes the states to be mutated here. Acceptable here only!!!
+        state.posts.push(action.payload); //immer JS makes the states to be mutated here. Acceptable here only!!!
       },
       prepare(title, content, userId) {
         return {
@@ -59,13 +73,14 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find((post) => post.id === postId);
+      const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
     },
   },
+  extraReducers(builder) {},
 });
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 export const { postAdded, reactionAdded } = postsSlice.actions;
 export default postsSlice.reducer;
