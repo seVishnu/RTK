5ebd2a294d415d992e0d88b9d88c00a11aeb1c59
vishnuSekhar <vishnusekhar96@gmail.com>
2022-12-13 17:27:24 +0530

    Add edit post form component

---
+++ src/features/posts/EditPostForm.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+
+const EditPostForm = () => {
+  return <div>EditPostForm</div>;
+};
+
+export default EditPostForm;
