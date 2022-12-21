cefae06fc2f682a1a98d48875246bf03a7475a7e
vishnuSekhar <vishnusekhar96@gmail.com>
2022-12-21 12:39:23 +0530

    Add user tab and functionalities

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
-
+import { useSelector, useDispatch } from "react-redux";
+import { increaseCount, getCount } from "../features/posts/postsSlice";
 const Header = () => {
+  const dispatch = useDispatch();
   return (
     <header>
       <h1>Redux Blog</h1>
@@ -11,6 +13,9 @@ const Header = () => {
           </li>
           <li>
             <Link to="post">Post</Link>
+          </li>
+          <li>
+            <Link to="user">User</Link>
           </li>
         </ul>
       </nav>
--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -5,7 +5,21 @@ import { Link, useParams } from "react-router-dom";
 const UserPage = () => {
   const { userId } = useParams();
   const user = useSelector((state) => selectUserById(state, Number(userId)));
-  return <div>UserPage</div>;
+  const postsForUser = useSelector((state) => {
+    const allPosts = selectAllPosts(state);
+    return allPosts.filter((post) => post.userId === Number(userId));
+  });
+  const postTitles = postsForUser.map((post) => (
+    <li key={post.id}>
+      <Link to={`/post/${post.id}`}>{post.title}</Link>
+    </li>
+  ));
+  return (
+    <section>
+      <h2>{user?.name}</h2>
+      <ol>{postTitles}</ol>
+    </section>
+  );
 };
 
 export default UserPage;
